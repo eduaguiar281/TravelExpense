fcebc6064175fed106a7b510d181f20de366f0d6
eduaguiar281 <eduaguiar281@hotmail.com>
2023-09-19 18:47:39 -0300

    Criação dos Testes de Integração

---
+++ tst/TravelExpense.Integration.Tests/SeedDatabase.sql
@@ -0,0 +1,30 @@
+﻿DELETE FROM Expenses
+DELETE FROM Travels
+
+SET IDENTITY_INSERT travels ON
+
+DECLARE @StartDateTime datetime2 = Getdate()-2
+DECLARE @EndDateTime datetime2 = Getdate()+2
+
+INSERT INTO Travels(Id, [Description], EmployeeRegistration, EmployeeName, StartedIn, EndedIn, [Status], TotalExpenses, CreatedAt, UpdatedAt)
+            Values (1, 'Viagem para visita cliente em Madrid', '22-56829-4', 'Eduardo Aguiar',@StartDateTime, @EndDateTime, 'Open', 0.00, getdate(), getdate())
+
+SET @StartDateTime = Getdate()+3
+SET @EndDateTime = Getdate()+6
+
+INSERT INTO Travels(Id, [Description], EmployeeRegistration, EmployeeName, StartedIn, EndedIn, [Status], TotalExpenses, CreatedAt, UpdatedAt)
+            Values (2, 'Viagem para visita cliente em Lisboa', '22-56829-4', 'Eduardo Aguiar',@StartDateTime, @EndDateTime, 'Open', 1500.00, getdate(), getdate())
+
+SET IDENTITY_INSERT travels OFF
+
+
+SET IDENTITY_INSERT Expenses ON
+
+
+INSERT INTO Expenses(Id, RelatedTo, [Description], VoucherId, [Value], [Date], [Status], Comments, TravelId, CreatedAt, UpdatedAt) 
+            Values  (1, 'Passagens aéreas', 'Passagens aérea AirFrance', null, 1500.00, @StartDateTime, 'Registered', 'A agencia possui apenas passagens classe vip', 2, getdate(), getdate())
+
+SET IDENTITY_INSERT Expenses OFF
+
+DBCC Checkident('travels', RESEED, 2)
+DBCC Checkident('expenses', RESEED, 1)
---
+++ tst/TravelExpense.Integration.Tests/TravelEndPointsTests.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using TravelExpense.Application.Commands.AproveExpense;
+using TravelExpense.Application.Commands.RegisterExpense;
+using TravelExpense.Application.Commands.RejectExpense;
+using TravelExpense.Application.Dtos;
+using TravelExpense.Core;
+using TravelExpense.Domain.Events;
+
+namespace TravelExpense.Integration.Tests
+{
+    public class TravelEndPointsTests : IClassFixture<TravelExpenseWebApplicationFactory>
+    {
+        private readonly TravelExpenseWebApplicationFactory _webAppFactory;
+        public TravelEndPointsTests(TravelExpenseWebApplicationFactory webAppFactory)
+        {
+            _webAppFactory = webAppFactory;
+        }
+
+        [Fact]
+        public async Task RegisterExpenseShouldBeCreatedCorrectly()
+        {
+            // Arrange
+            HttpClient? client = _webAppFactory.CreateClient();
+            await _webAppFactory.SeedDatabase();
+
+            var command = new RegisterExpenseCommand()
+            {
+                RelatedTo = "Hospedagem em Hotel",
+                Description = "05 diárias no Hotel Astron Suítes",
+                Date = DateTime.Now.AddDays(1),
+                Value = 2550.65m
+            };
+            string body = JsonConvert.SerializeObject(command);
+            var stringContent = new StringContent(body, Encoding.UTF8, "application/json");
+
+            // Act
+            var registerResponse = await client.PostAsync("travels/1/expenses", stringContent);
+            var travelResponse =  await client.GetFromJsonAsync<QueryResult<TravelDto>>("travels/1");
+            var expenseResponse = await client.GetFromJsonAsync<QueryResult<ExpenseDto>>($"travels/1/expenses/{travelResponse!.Data!.Expenses.FirstOrDefault()!.Id}");
+
+            var @event = _webAppFactory.AmqpClient.ListenFirst<ExpenseRegisteredEvent>(nameof(ExpenseRegisteredEvent));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, registerResponse.StatusCode);
+            Assert.Equal(2550.65m, travelResponse!.Data!.TotalExpenses);
+            Assert.Equal(command.RelatedTo, expenseResponse!.Data!.RelatedTo);
+            Assert.Equal(command.Description, expenseResponse!.Data!.Description);
+            Assert.Equal(command.Date, expenseResponse!.Data!.Date);
+            Assert.Equal(command.Value, expenseResponse!.Data!.Value);
+
+            Assert.Equal(command.RelatedTo, @event!.RelatedTo);
+            Assert.Equal(command.Description, @event!.Description);
+            Assert.Equal(command.Date, @event!.Date);
+            Assert.Equal(command.Value, @event!.Value);
+        }
+
+        [Fact]
+        public async Task AproveExpenseShouldBeAprovedCorrectly()
+        {
+            // Arrange
+            HttpClient? client = _webAppFactory.CreateClient();
+            await _webAppFactory.SeedDatabase();
+            var command = new AproveExpenseCommand()
+            {
+                Id = 2,
+                ExpenseId = 1,
+                VoucherId = "X-123-4b",
+                Comment = "Aprovado. Centro de Custos 10.001.0099- TI Eventos e Palestras"
+            };
+            string body = JsonConvert.SerializeObject(command);
+            var stringContent = new StringContent(body, Encoding.UTF8, "application/json");
+
+            // Act
+            var aproveResponse = await client.PutAsync("/travels/2/expenses/1/aprove", stringContent);
+            var expenseResponse = await client.GetFromJsonAsync<QueryResult<ExpenseDto>>("/travels/2/expenses/1");
+            var @event = _webAppFactory.AmqpClient.ListenFirst<ExpenseAprovedEvent>(nameof(ExpenseAprovedEvent));
+
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, aproveResponse.StatusCode);
+            Assert.Equal(command.ExpenseId, expenseResponse!.Data!.Id);
+            Assert.Equal(command.VoucherId, expenseResponse!.Data!.VoucherId);
+            Assert.Contains(command.Comment, expenseResponse!.Data!.Comments);
+
+            Assert.Equal(command.ExpenseId, @event!.ExpenseId);
+            Assert.Equal(command.VoucherId, @event!.VoucherId);
+            Assert.Equal(command.Comment, @event!.Comment);
+        }
+
+        [Fact]
+        public async Task RejectExpenseShouldBeRejectedCorrectly()
+        {
+            // Arrange
+            HttpClient? client = _webAppFactory.CreateClient();
+            await _webAppFactory.SeedDatabase();
+            var command = new RejectExpenseCommand()
+            {
+                Id = 2,
+                ExpenseId = 1,
+                Comment = "Aprovado. Centro de Custos 10.001.0099- TI Eventos e Palestras"
+            };
+            string body = JsonConvert.SerializeObject(command);
+            var stringContent = new StringContent(body, Encoding.UTF8, "application/json");
+
+            // Act
+            var rejectResponse = await client.PutAsync("/travels/2/expenses/1/reject", stringContent);
+            var expenseResponse = await client.GetFromJsonAsync<QueryResult<ExpenseDto>>("/travels/2/expenses/1");
+            var @event = _webAppFactory.AmqpClient.ListenFirst<ExpenseRejectedEvent>(nameof(ExpenseRejectedEvent));
+
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, rejectResponse.StatusCode);
+            Assert.Equal(command.ExpenseId, expenseResponse!.Data!.Id);
+            Assert.Contains(command.Comment, expenseResponse!.Data!.Comments);
+
+            Assert.Equal(command.ExpenseId, @event!.ExpenseId);
+            Assert.Equal(command.Comment, @event!.Comment);
+        }
+
+    }
+}
